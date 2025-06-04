f9a1654d9dd6d3560daa7cb7ef64c67050a93798
maodzhedun <48294837+maodzhedun@users.noreply.github.com>
2025-06-04 11:28:44 +0300

    fix1

--- src/components/Searchbar/Searchbar.styled.jsx
+++ src/components/Searchbar/Searchbar.styled.jsx
@@ -34,7 +34,7 @@ export const SearchFormButton = styled.button`
   width: 48px;
   height: 48px;
   border: 0;
-  background-image: url('https://image.flaticon.com/icons/svg/149/149852.svg');
+  background-image: url('https://img.icons8.com/?size=100&id=131&format=png&color=000000');
   background-size: 40%;
   background-repeat: no-repeat;
   background-position: center;
